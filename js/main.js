29a8eaecbff259e4778f9f744cbca81acd6142b8
Xemsssa <Xemsss@gmail.com>
2018-10-02 17:27:21 +0300

    our clients block with owl-slider

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        lazyLoad:true,
+        // items:1,
+        autoplay: true,
+        smartSpeed: 1500,
+        loop: true,
+        // margin: 10,
+        dots: false,
+        // responsiveClass: true,
+        // singleItem:true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+            },
+            600: {
+                items: 2,
+                nav: true
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                // loop: false
+            }
+        }
+    })
+});
